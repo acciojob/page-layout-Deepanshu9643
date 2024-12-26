02972bfba90e37611e9da66cd3a6ad7c02edfeb8
itsNeer <Deepanshukhorwal7@gmail.com>
2024-12-27 00:56:28 +0530

    initial change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
+
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="header">
+        <h1>Welcome to My Website</h1>
+      </div>
+      <div className="content">
+        <h2>Page Content</h2>
+        <p>This is the main content of the page.</p>
+      </div>
+      <div className="footer">
+        <p>© 2024 My Website</p>
+      </div>
     </div>
   )
 }
